78dfc45b35aa52420c4436bb4804690c77270e1f
yvp <yvanpraag@gmail.com>
2025-01-30 19:12:38 +0100

    save-> close

--- streamlit_main.py
+++ streamlit_main.py
@@ -15,7 +15,7 @@ def to_excel(df):
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
+        writer.close()
     processed_data = output.getvalue()
     return processed_data
 
